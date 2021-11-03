10b49d5367f0bd8c3089ca6dffa90c09988020b0
vmadmin <vmadmin@BMWP1>
2021-11-03 16:35:16 +0100

    Add project files.

---
+++ Logic/Calculate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Calculate
+    {
+        public static float calc(int zahl1, int zahl2, OperatorEnum op)
+        {
+            switch (op)
+            {
+                case OperatorEnum.plus:
+                    return zahl1 + zahl2;
+                    break;
+                case OperatorEnum.minus:
+                    return zahl1 - zahl2;
+                    break;
+                case OperatorEnum.mal:
+                    return zahl1 * zahl2;
+                    break;
+                case OperatorEnum.durch:
+                    return zahl1 / zahl2;
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+        }
+    }
+}
---
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using Logic;
+
+namespace SimpleCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            txtResultat.Text = (Calculate.calc(Convert.ToInt32(txtZahl1.Text), Convert.ToInt32(txtZahl2.Text), OperatorEnum.plus)).ToString();
+            btnAbspeichern.IsEnabled = true;
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            txtResultat.Text = (Calculate.calc(Convert.ToInt32(txtZahl1.Text), Convert.ToInt32(txtZahl2.Text), OperatorEnum.minus)).ToString();
+            btnAbspeichern.IsEnabled = true;
+        }
+
+        private void btnMal_Click(object sender, RoutedEventArgs e)
+        {
+            txtResultat.Text = (Calculate.calc(Convert.ToInt32(txtZahl1.Text), Convert.ToInt32(txtZahl2.Text), OperatorEnum.mal)).ToString();
+            btnAbspeichern.IsEnabled = true;
+        }
+
+        private void btnDurch_Click(object sender, RoutedEventArgs e)
+        {
+            txtResultat.Text = (Calculate.calc(Convert.ToInt32(txtZahl1.Text), Convert.ToInt32(txtZahl2.Text), OperatorEnum.durch)).ToString();
+            btnAbspeichern.IsEnabled = true;
+        }
+
+        private void txtZahl1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void txtZahl2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
